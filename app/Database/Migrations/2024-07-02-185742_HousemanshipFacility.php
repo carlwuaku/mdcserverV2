54ef132c70e28953a34c0fd83e0303dc11b4e64f
wuakuc <wuakuc@gmail.com>
2024-07-03 08:06:18 +0100

    add housemanship module migrations for tables and settings

---
+++ app/Database/Migrations/2024-07-02-185742_HousemanshipFacility.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+use CodeIgniter\Database\RawSql;
+
+class HousemanshipFacility extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 9,
+                'auto_increment' => true,
+            ],
+            'uuid' => [
+                'type' => 'CHAR',
+                'constraint' => 36,
+                'null' => false,
+                'unique' => true,
+            ],
+            'name' => [
+                'type' => 'TEXT',
+                'null' => true,
+                'default' => null
+            ],
+            'region' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'null' => false
+            ],
+            'location' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'null' => true,
+                'default' => null
+            ],
+            'type' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'null' => true,
+                'default' => null,
+            ],
+
+
+            'phone' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'null' => true,
+                'default' => null
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '500',
+                'null' => true,
+                'default' => null
+            ],
+
+
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+                'default' => null
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+                'default' => null
+            ],
+
+
+            'created_on' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+                'default' => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+            'modified_on' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+                'default' => new RawSql('CURRENT_TIMESTAMP'),
+                'on_update' => new RawSql('CURRENT_TIMESTAMP'),
+            ]
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addKey('name', false, true);
+        $this->forge->addKey('type', false, true);
+        $this->forge->addKey('region', false, true);
+        
+        $this->forge->createTable('housemanship_facilities', true);
+        $trigger = "
+       CREATE TRIGGER before_insert_housemanship_facility
+       BEFORE INSERT ON housemanship_facilities
+       FOR EACH ROW
+       BEGIN
+        SET NEW.uuid = UUID();
+       END;
+       ";
+        $this->db->query($trigger);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
